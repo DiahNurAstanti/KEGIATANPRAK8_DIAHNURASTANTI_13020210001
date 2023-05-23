35e049fc8e57944081808e0d44a220030cfc1d4b
DIAH NUR ASTANTI <127530115+DiahNurAstanti@users.noreply.github.com>
2023-05-24 00:08:46 +0800

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package model;
+
+
+public class Book {
+
+   public int id;
+   private String authorName;
+   private String title;
+   
+   public Book(String authorName, String title){
+       this.title = title;
+       this.authorName = authorName;
+   }
+
+    public Book() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
